dea900a60c788ef805de4429a4133c26333ed8e8
Nikita Pyshkov <85805562+pyshkovni@users.noreply.github.com>
2023-04-26 10:45:00 +0300

    Проверил и добавил комментарий

--- homework.py
+++ homework.py
@@ -4,3 +4,7 @@ for i in range(len(s)-1):
     if ord(s[i]) > ord(s[i+1]):
         is_alphabetical = False
 print(is_alphabetical)
+
+# Супер)
+# Теперь наша задача преобразовать данный код в функцию.
+# Новое решение нужно добавить с помощью git с локального устройства
