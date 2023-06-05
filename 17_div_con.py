e6a3fcb4ed8372968dc4680413b5314b73dc1b74
OreshkinEM <124786400+OreshkinEM@users.noreply.github.com>
2023-06-05 14:17:27 +0300

    Решил задание без функции

--- 17_div_con.py
+++ 17_div_con.py
@@ -13,3 +13,23 @@
 # НАДА:
 # - в этом файле решить задачу (без функции)
 # - в другом файле .py решить эту же задачу с функцией
+
+# без функции
+# Входные данные
+
+# data = [9, 3, '7', '3']
+# data = ['5', '0', 9, 3, 2, 1, '9', 6, 7]
+data = ['3', 6, 6, 0, '5', 8, 5, '6', 2,'0']
+
+data_int = 0
+data_str = 0
+
+for i in data:
+    if type(i) is int:
+        data_int += i
+    elif type(i) is str:
+        data_str += int(i)
+
+answer = data_int - data_str
+
+print(answer)
